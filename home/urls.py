7c6e2d22cd7a84cb28d3e27a22d6641c7616504a
mugambi645 <mugambi645@gmail.com>
2023-03-22 23:46:41 +0300

    create first view

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    
+    path("", views.index, name="index"),
 ]
--- library/settings.py
+++ library/settings.py
@@ -129,11 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
